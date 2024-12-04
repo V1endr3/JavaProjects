458cb786d32c9e3a8f2de2cc102b929da064d7b6
v1endr3 <xiayuanming4m@gmail.com>
2024-12-04 17:13:15 +0800

    modify [Camel]: => os & es

--- Camel/src/main/java/org/example/MainApplication.java
+++ Camel/src/main/java/org/example/MainApplication.java
@@ -1,15 +1,11 @@
 package org.example;
 
 import org.apache.camel.spring.Main;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class MainApplication {
     public static void main(String[] args) throws Exception {
+        String[] argList = {"-fa", "file:Camel/conf/test.xml"};
         Main main = new Main();
-        FileSystemXmlApplicationContext applicationContext = new FileSystemXmlApplicationContext("file:Camel/conf/test.xml");
-        main.setApplicationContext(applicationContext);
-
-        main.run();
-        main.getCamelContext().setDevConsole(true);
+        main.run(argList);
     }
 }
