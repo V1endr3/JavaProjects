d91c252938fc4c87e5c96bc2e329415b4f4ea11e
v1endr3 <xiayuanming4m@gmail.com>
2025-01-02 09:26:27 +0800

    init [Guava]

---
+++ Guava/src/test/java/org/example/ArrayTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import com.google.common.collect.Lists;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ArrayTest {
+    @Test
+    public void testLists() {
+        List<Object> testArray = Lists.newArrayList();
+    }
+}
