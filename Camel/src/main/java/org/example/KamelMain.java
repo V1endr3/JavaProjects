9feb1089151d2c4ad5344ed8d632cf977426aa03
v1endr3 <xiayuanming4m@gmail.com>
2025-03-05 16:59:05 +0800

    modify [Camel]: => Runtime Route runner

--- Camel/src/main/java/org/example/KamelMain.java
+++ Camel/src/main/java/org/example/KamelMain.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.main.KameletMain;
 
+@Slf4j
 public class KamelMain {
     public static void main(String[] args) throws Exception {
         KameletMain main = new KameletMain("orchestration");
         main.configure().withRoutesIncludePattern("file:Camel/conf/test.xml");
         main.run(args);
+        
+        log.info("Endpoints size: {}", main.getCamelContext().getEndpoints().size());
     }
 }
--- Camel/src/main/java/org/example/runner/DemoRunner.java
+++ Camel/src/main/java/org/example/runner/DemoRunner.java
@@ -1,14 +1,78 @@
 package org.example.runner;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.Endpoint;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.Route;
+import org.apache.camel.component.rest.RestEndpoint;
+import org.apache.camel.main.KameletMain;
+import org.apache.camel.spi.CamelEvent;
+import org.apache.camel.spi.RouteController;
+import org.apache.camel.support.EventNotifierSupport;
+import org.apache.camel.support.ScheduledPollEndpoint;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 @Slf4j
 public class DemoRunner {
-    public static void main(String[] args) {
-        String location = "route1";
+    public static void main(String[] args) throws Exception {
+        String filepath = "file:Camel/conf/test.xml";
+        boolean hasRest = false;
+        Endpoint restEndpoint = null;
+        // initialize main
+        KameletMain main = new KameletMain("orchestration");
+        main.configure().withRoutesIncludePattern(filepath);
+        main.start();
 
-        String xmlContent = "<route id=\"route1\"><from uri=\"direct:mock-start\"/><to uri=\"log:end\"/></route>";
-        
-        System.out.println(xmlContent);
+        RouteController routeController = main.getCamelContext().getRouteController();
+        routeController.stopAllRoutes();
+        List<Route> routeList = main.getCamelContext().getRoutes();
+        for (Route route : routeList) {
+            Endpoint startPoint = route.getEndpoint();
+            if (startPoint instanceof ScheduledPollEndpoint) {
+                ((ScheduledPollEndpoint) startPoint).setRepeatCount(1);
+                String scheduler = (String) ((ScheduledPollEndpoint) startPoint).getScheduler();
+                if ("none".equals(scheduler)) {
+                    ((ScheduledPollEndpoint) startPoint).setDelay(0);
+                    ((ScheduledPollEndpoint) startPoint).setInitialDelay(0);
+                } else if ("quartz".equals(scheduler)) {
+                    ((ScheduledPollEndpoint) startPoint).setScheduler("quartz");
+                    Map<String, Object> props = new HashMap<>();
+                    props.put("cron", "0/1 * * * * ?");
+                    ((ScheduledPollEndpoint) startPoint).setSchedulerProperties(props);
+                } else {
+                    throw new IllegalArgumentException("不支持的调度类型: " + scheduler);
+                }
+            } else if (startPoint instanceof RestEndpoint) {
+                restEndpoint = startPoint;
+                hasRest = true;
+                log.info("test");
+            } else {
+                log.error("不支持的试运行类型：{}", startPoint.getClass().getName());
+                throw new IllegalArgumentException("不支持的试运行类型");
+            }
+        }
+        routeController.startAllRoutes();
+
+        CountDownLatch latch = new CountDownLatch(1);
+        main.getCamelContext().getManagementStrategy().addEventNotifier(new EventNotifierSupport() {
+            @Override
+            public void notify(CamelEvent event) throws Exception {
+                if (event instanceof CamelEvent.ExchangeCompletedEvent) {
+                    latch.countDown();
+                }
+            }
+        });
+
+        if (hasRest) {
+            ProducerTemplate producerTemplate = main.getCamelContext().createProducerTemplate();
+            producerTemplate.sendBody(restEndpoint, "hello");
+        }
+
+        latch.await();
+        main.stop();
     }
 }
