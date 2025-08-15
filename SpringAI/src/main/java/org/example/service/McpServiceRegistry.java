66d058699cae594d9b9ab66556ea5040da92168c
V1endr3 <xiayuanming4m@gmail.com>
2025-08-15 14:50:50 +0800

    modify [Spring AI]: add comments

--- SpringAI/src/main/java/org/example/service/McpServiceRegistry.java
+++ SpringAI/src/main/java/org/example/service/McpServiceRegistry.java
@@ -17,12 +17,40 @@ public class McpServiceRegistry {
     private McpSyncServer mcpSyncServer;
 
     public void registerTool(String name, String description, String inputJsonSchema, String outputContent) {
+        /**
+         * create definition
+         *
+         * name: key,
+         * description: description,
+         * inputJsonSchema: {
+         *     "type": "object",
+         *     "properties": {
+         *         "path": {},
+         *         "query": {},
+         *         "body": {}
+         *     }
+         * }
+         */
         var mcpTool = new McpSchema.Tool(name, description, inputJsonSchema);
 
-        BiFunction<McpSyncServerExchange, Map<String, Object>, McpSchema.CallToolResult> toolFunction = (exchange, invocation) -> McpSchema.CallToolResult.builder()
-                .isError(false)
-                .addTextContent(outputContent)
-                .build();
+        /**
+         * process the tool invocation
+         *
+         * McpSyncServerExchange => xxx
+         * Map<String, Object> invocation => AI根据JSON Schema生成的参数 {"path": {}, "query": {}, "body": {}}
+         * McpSchema.CallToolResult => 返回结果类型
+         */
+        BiFunction<McpSyncServerExchange, Map<String, Object>, McpSchema.CallToolResult> toolFunction =
+                (exchange, invocation) -> {
+            // todo:
+            // RestClient做接口请求, 用 invocation 中的参数进行填充
+                    Object body = invocation.get("body");
+                    Object result = null;
+                    return McpSchema.CallToolResult.builder()
+                            .isError(false)
+                            .addTextContent(outputContent)
+                            .build();
+        };
 
         var newTool = new McpServerFeatures.SyncToolSpecification(mcpTool, toolFunction);
 
